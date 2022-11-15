091780602a96052ec77f8a4f8d0e37fdfc198380
Flooooooooooorian <florian.florianweber.weber@googlemail.com>
2022-11-15 16:39:14 +0100

    Add Student/Rick&Morty Type

--- Student.ts
+++ Student.ts
@@ -1,17 +1,24 @@
-const student1: { name: String, id: String } = {
+type Student = {
+    name: String,
+//? für Optionale Attribute, | für mehrere mögliche Typen
+    id?: String | number
+}
+
+
+const student1: Student = {
     name: "Florian",
     id: "123"
 }
 
-const student2: { name: String, id: String } = {
+const student2: Student = {
     name: "Matthias",
     id: "456"
 }
 
-const students: { name: String; id: String }[] = [student1, student2]
+const students: Student[] = [student1, student2]
 
 
-const updateName = (students: { name: String, id: String }[], id: String, newName: String): { name: String, id: String }[] => {
+const updateName = (students: Student[], id: String, newName: String): Student[] => {
     return students.map((student) => {
         if (student.id === id) {
             student.name = newName
@@ -30,10 +37,10 @@ console.log(newList)
 
 
 
-const updateNameAndCopy = (students: { name: String, id: String }[], id: String, newName: String): { name: String, id: String }[] => {
+const updateNameAndCopy = (students: Student[], id: String, newName: String): Student[] => {
     return students.map((student) => {
         if (student.id === id) {
-            const newStudent: { name: String, id: String } = {name: newName, id: student.id}
+            const newStudent: Student = {name: newName, id: student.id}
             return newStudent
         }
         return student
@@ -47,3 +54,7 @@ const copyList = updateNameAndCopy(students, "123", "Florian")
 
 console.log(students)
 console.log(copyList)
+
+const student3: Student = {name: "Max Mustermann"}
+const student4: Student = {name: "Max Mustermann", id: "4"}
+const student5: Student = {name: "Max Mustermann", id: 5}
